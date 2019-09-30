ac9721e0753c25f347cb0e2ebe60e97c281638b6
HuangshuiFish <1782045704@qq.com>
2019-09-30 15:45:10 +0800

    验证书中的例题

---
+++ practice12/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;//标准库所提供的任何事物都被封装在命名空间std中。
+
+int main()
+{
+    string user_name;
+    cin >> user_name;
+    cout << "\n" << user_name << endl;
+    return 0;
+}
