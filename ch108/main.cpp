abb9f88b2656923fc396a057c52072b3e3d0eae9
HuangshuiFish <1782045704@qq.com>
2019-10-08 19:36:42 +0800

    将pass by value转变为pass by reference，冒泡程序可以正常运行，具体可参考书P45

--- ch108/main.cpp
+++ ch108/main.cpp
@@ -11,14 +11,14 @@ void display( vector<int> vec )
     cout << endl;
 }
 
-void swap(int val1, int val2)
+void swap(int &val1, int &val2)//前面加的&是reference，将参数声明为一个reference，具体可以参考书《Essential C++》P45的内容
 {
     int temp = val1;
     val1 = val2;
     val2 = temp;
 }
 
-void bubble_sort(vector<int> vec)
+void bubble_sort(vector<int> &vec)//同上
 {
     for (int ix = 0; ix < vec.size(); ++ix)
         for (int jx = ix + 1; jx < vec.size(); ++jx)
